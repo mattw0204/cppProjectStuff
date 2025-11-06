15faf79b69a171d703f0074215a4a2af057232f4
mattw0204 <45386400+mattw0204@users.noreply.github.com>
2025-11-06 12:22:19 +0000

    Created Struct

---
+++ Source/testr/Public/EntityStatStruct.h
@@ -0,0 +1,22 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EntityStatStruct.generated.h"
+
+USTRUCT(BlueprintType)
+struct FEntityStatStruct
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float health;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float maxHealth;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float stamina;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float maxStamina;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float speed;
+};
